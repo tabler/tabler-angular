28e359a47b64396aed05e215b44de0d42784f63b
Bram Borggreve <borggreve@gmail.com>
2018-04-08 01:29:51 -0500

    fix(angular-ui): show header navigation on small screens

--- libs/angular-ui/src/components/header/header.component.ts
+++ libs/angular-ui/src/components/header/header.component.ts
@@ -23,13 +23,13 @@ import { Component, Input } from '@angular/core'
       </div>
     </div>
 
-    <div class="header-nav d-none d-lg-flex">
+    <div class="header-nav">
       <div class="container">
         <div class="row align-items-center">
           <div class="col">
             <ui-menu [links]="config?.menu"></ui-menu>
           </div>
-          <div class="col-3 ml-auto">
+          <div class="col-3 ml-auto" *ngIf="config?.search">
             <form class="input-icon">
               <input type="search" class="form-control header-search" placeholder="Search&hellip;" tabindex="1">
               <div class="input-icon-addon">
