98077b0a67bbe8cacbc4d2d266c75c64f7f0027f
Bram Borggreve <borggreve@gmail.com>
2018-04-09 22:07:34 -0500

    feat(core): export Forms, Modal from CoreModule

--- libs/angular-core/src/shared.module.ts
+++ libs/angular-core/src/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { UiModule } from '@tabler/angular-ui'
+import { ModalModule } from 'ngx-bootstrap'
 
 @NgModule({
   imports: [
     CommonModule,
     UiModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule,
   ],
   exports: [
     CommonModule,
     UiModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule,
   ],
 })
 export class SharedModule {
