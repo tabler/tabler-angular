21ef27187cd25b94706aba9695e57c249f171e1c
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2018-04-29 17:02:52 +0300

    Refactor to templateUrl gallery's components

--- apps/demo/src/app/gallery/components/gallery-list/gallery-list.component.ts
+++ apps/demo/src/app/gallery/components/gallery-list/gallery-list.component.ts
@@ -2,13 +2,7 @@ import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-gallery-list',
-  template: `
-    <div class="row row-cards">
-      <div class="col-sm-6 col-lg-4" *ngFor="let photo of photos; let index = index">
-        <app-gallery-item [photo]="photo" [user]="users[index]" [index]="index"></app-gallery-item>
-      </div>
-    </div>
-  `,
+  templateUrl: './gallery-list.html',
   styles: [],
 })
 export class GalleryListComponent {
