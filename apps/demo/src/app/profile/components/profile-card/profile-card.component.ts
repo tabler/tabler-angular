e4ff867b0fa2767caa1202b0fa759d75c9a7de3b
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2018-04-29 17:53:38 +0300

    Refactor to templateUrl profile's components

--- apps/demo/src/app/profile/components/profile-card/profile-card.component.ts
+++ apps/demo/src/app/profile/components/profile-card/profile-card.component.ts
@@ -2,24 +2,7 @@ import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-profile-card',
-  template: `
-    <div class="card card-profile">
-      <div class="card-header" [style.background-image]="backgroundImage"></div>
-      <div class="card-body text-center">
-        <img class="card-profile-img" src="{{ user.photo }}">
-
-        <h3 class="mb-3">{{ user.name }} {{ user.surname }}</h3>
-
-        <p class="mb-4">
-          Big belly rude boy, million dollar hustler. Unemployed.
-        </p>
-
-        <button class="btn btn-outline-primary btn-sm">
-          <span class="fa fa-twitter"></span> Follow
-        </button>
-      </div>
-    </div>
-  `,
+  templateUrl: './profile-card.html',
   styles: [],
 })
 export class ProfileCardComponent {
